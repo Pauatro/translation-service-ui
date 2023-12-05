38aea5f2b47dec429760c35f997bd0658df57f6b
Pauatro <pauatro@gmail.com>
2023-12-05 14:32:31 +0100

    feat: add translations api

---
+++ src/shared/api/index.ts
@@ -0,0 +1,49 @@
+export enum ApiMethod {
+  GET = "GET",
+  POST = "POST",
+  PATCH = "PATCH",
+  PUT = "PUT",
+  DELETE = "DELETE",
+}
+
+type OptionalReturn<T> = T extends undefined ? void : T;
+
+interface FetchApiParams {
+  method: ApiMethod;
+  url: string;
+  headers?: Record<string, string>;
+  data?: unknown;
+  locale?: string;
+}
+
+export const fetchApi = async <T = undefined>({
+  method,
+  url,
+  headers = {},
+  data,
+}: FetchApiParams): Promise<OptionalReturn<T>> => {
+  const defaultHeaders: Record<string, string> = {
+    "Content-Type": "application/json",
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: method,
+      headers: {
+        ...defaultHeaders,
+        ...headers,
+      },
+      body: data ? JSON.stringify(data) : null,
+    });
+
+    const responseText = await response.text();
+
+    if (responseText) {
+      return JSON.parse(responseText) as OptionalReturn<T>;
+    }
+
+    return undefined as OptionalReturn<T>;
+  } catch (e) {
+    // TODO: manage API errors
+  }
+};
---
+++ src/translations/api/index.ts
@@ -0,0 +1,60 @@
+import { ApiMethod, fetchApi } from "../../shared/api";
+import { environmentService } from "../../shared/environmentService";
+
+interface TranslateToJeringozaRequestBody {
+  text: string;
+}
+
+interface TranslateToLanguageRequestBody {
+  text: string;
+  language: string;
+}
+
+interface TranslationEndpointResponse {
+  data: string;
+}
+
+interface AvailableLanguagesEndpointResponse {
+  data: string[];
+}
+
+export class TranslationsApi {
+  private static baseUrl = environmentService.translationApiBaseUrl;
+  private static translationsApiPrefix = "/translations";
+  private static translationsApiEndpoints = {
+    jeringoza: `${this.baseUrl}${this.translationsApiPrefix}/jeringoza`,
+    languageTranslation: `${this.baseUrl}${this.translationsApiPrefix}`,
+    fetchLanguages: `${this.baseUrl}${this.translationsApiPrefix}/languages`,
+  };
+
+  static async translateToJeringoza(text: string) {
+    const translation = await fetchApi<TranslationEndpointResponse>({
+      url: this.translationsApiEndpoints.jeringoza,
+      method: ApiMethod.POST,
+      data: {
+        text,
+      } as TranslateToJeringozaRequestBody,
+    });
+    return translation.data;
+  }
+
+  static async translateToLanguage(language: string, text: string) {
+    const translation = await fetchApi<TranslationEndpointResponse>({
+      url: this.translationsApiEndpoints.languageTranslation,
+      method: ApiMethod.POST,
+      data: {
+        language,
+        text,
+      } as TranslateToLanguageRequestBody,
+    });
+    return translation.data;
+  }
+
+  static async fetchAvailableLanguages() {
+    const translation = await fetchApi<AvailableLanguagesEndpointResponse>({
+      url: this.translationsApiEndpoints.fetchLanguages,
+      method: ApiMethod.GET,
+    });
+    return translation.data;
+  }
+}
